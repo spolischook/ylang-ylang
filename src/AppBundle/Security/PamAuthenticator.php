033c6f9c48f83b14ed808ecac74e58cc622d14ae
spolischook <spolischook@gmail.com>
2015-11-20 11:37:44 +0200

    Added tests and refactoring for PamAuthenticator

--- src/AppBundle/Security/PamAuthenticator.php
+++ src/AppBundle/Security/PamAuthenticator.php
@@ -6,9 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\SimpleFormAuthenticatorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class PamAuthenticator implements SimpleFormAuthenticatorInterface
@@ -32,13 +30,8 @@ class PamAuthenticator implements SimpleFormAuthenticatorInterface
      */
     public function authenticateToken(TokenInterface $token, UserProviderInterface $userProvider, $providerKey)
     {
-        if (pam_auth($token->getUsername(), $token->getCredentials())) {
-            return new UsernamePasswordToken(
-                $token->getUsername(),
-                $token->getCredentials(),
-                $providerKey,
-                in_array($token->getUsername(), $this->rootUsers) ? ['ROLE_USER', 'ROLE_ADMIN'] : ['ROLE_USER']
-            );
+        if (true === $this->pamAuth($token->getUsername(), $token->getCredentials())) {
+            return $token;
         }
 
         throw new AuthenticationException(
@@ -67,6 +60,16 @@ class PamAuthenticator implements SimpleFormAuthenticatorInterface
      */
     public function createToken(Request $request, $username, $password, $providerKey)
     {
-        return new UsernamePasswordToken($username, $password, $providerKey);
+        return new UsernamePasswordToken($username, $password, $providerKey, in_array($username, $this->rootUsers) ? ['ROLE_USER', 'ROLE_ADMIN'] : ['ROLE_USER']);
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @return bool
+     */
+    public function pamAuth($username, $password)
+    {
+        return pam_auth($username, $password);
     }
 }
---
+++ src/AppBundle/Tests/Security/PamAuthenticatorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Tests\Security;
+
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\InMemoryUserProvider;
+
+class PamAuthenticatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAuthenticateValidToken()
+    {
+        $pamAuthenticatorMock = $this->getPamMock(true);
+
+        $this->assertInstanceOf('Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken', $pamAuthenticatorMock->authenticateToken(
+            new UsernamePasswordToken('test', 'test', 'main', []), new InMemoryUserProvider(), 'main'
+        ));
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Security\Core\Exception\AuthenticationException
+     */
+    public function testAuthenticateNotValidToken()
+    {
+        $pamAuthenticatorMock = $this->getPamMock(false);
+
+        $this->assertInstanceOf('Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken', $pamAuthenticatorMock->authenticateToken(
+            new UsernamePasswordToken('test', 'test', 'main', []), new InMemoryUserProvider(), 'main'
+        ));
+    }
+
+    /**
+     * @param bool $isValidToken
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getPamMock($isValidToken)
+    {
+        $pamAuthenticatorMock = $this->getMockBuilder('AppBundle\Security\PamAuthenticator')
+            ->setConstructorArgs([[]])
+            ->setMethods(['pamAuth'])
+            ->getMock();
+        $pamAuthenticatorMock->method('pamAuth')->willReturn($isValidToken);
+
+        return $pamAuthenticatorMock;
+    }
+}

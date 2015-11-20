55812adf7515482c39c37ec3f209fa1fd796d25c
spolischook <spolischook@gmail.com>
2015-11-21 00:48:28 +0200

    Extends LogParser

---
+++ src/AppBundle/Parser/LogParser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Parser;
+
+use Kassner\LogParser\LogParser as BaseLogParser;
+use Kassner\LogParser\FormatException;
+use AppBundle\Entity\Log;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+class LogParser extends BaseLogParser
+{
+    /**
+     * Parses one single log line
+     *
+     * @param string $line
+     * @return Log
+     * @throws FormatException
+     */
+    public function parse($line)
+    {
+        if (!preg_match($this->pcreFormat, $line, $matches)) {
+            throw new FormatException($line);
+        }
+
+        $entry = new Log();
+        $propertyAccessor = new PropertyAccessor();
+
+        foreach (array_filter(array_keys($matches), 'is_string') as $key) {
+            if ('time' === $key && true !== $stamp = strtotime($matches[$key])) {
+                $entry->setStamp($stamp);
+            }
+
+            $propertyAccessor->setValue($entry, $key, $matches[$key]);
+        }
+
+        return $entry;
+    }
+}

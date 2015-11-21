23f8ab39091e46d73ba4b703fbb34a5f31ca4de7
spolischook <spolischook@gmail.com>
2015-11-22 01:49:57 +0200

    minor. Remove uninformative comment

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -12,7 +12,6 @@ class AdminController extends Controller
      */
     public function indexAction()
     {
-        // replace this example code with whatever you need
         return $this->render('admin/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));

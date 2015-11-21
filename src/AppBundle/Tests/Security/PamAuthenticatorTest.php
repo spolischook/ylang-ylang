e7e426194ac1dc7117027615cfb4cd6fade27adf
spolischook <spolischook@gmail.com>
2015-11-21 23:01:06 +0200

    Updated tests

--- src/AppBundle/Tests/Security/PamAuthenticatorTest.php
+++ src/AppBundle/Tests/Security/PamAuthenticatorTest.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Tests\Security;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\User\InMemoryUserProvider;
 
@@ -9,7 +10,7 @@ class PamAuthenticatorTest extends \PHPUnit_Framework_TestCase
 {
     public function testAuthenticateValidToken()
     {
-        $pamAuthenticatorMock = $this->getPamMock(true);
+        $pamAuthenticatorMock = $this->getPamAuthenticatorMock(true);
 
         $this->assertInstanceOf('Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken', $pamAuthenticatorMock->authenticateToken(
             new UsernamePasswordToken('test', 'test', 'main', []), new InMemoryUserProvider(), 'main'
@@ -21,18 +22,37 @@ class PamAuthenticatorTest extends \PHPUnit_Framework_TestCase
      */
     public function testAuthenticateNotValidToken()
     {
-        $pamAuthenticatorMock = $this->getPamMock(false);
+        $pamAuthenticatorMock = $this->getPamAuthenticatorMock(false);
 
         $pamAuthenticatorMock->authenticateToken(
             new UsernamePasswordToken('test', 'test', 'main', []), new InMemoryUserProvider(), 'main'
         );
     }
 
+    public function testCreateToken()
+    {
+        $this->assertInstanceOf('Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken',
+            $this->getPamAuthenticatorMock(true)->createToken(new Request(), 'test_login', 'test_pass', 'provider'));
+    }
+
+    public function testSupportsToken()
+    {
+        $pamAuthenticatorMock = $this->getPamAuthenticatorMock(false);
+
+        $token = new UsernamePasswordToken('test_login', 'test_pass', 'provider1');
+        $this->assertTrue($pamAuthenticatorMock->supportsToken($token, 'provider1'));
+        $this->assertFalse($pamAuthenticatorMock->supportsToken($token, 'provider2'));
+
+        $token = new UsernamePasswordToken('test_login', 'test_pass', 'provider2');
+        $this->assertFalse($pamAuthenticatorMock->supportsToken($token, 'provider1'));
+        $this->assertTrue($pamAuthenticatorMock->supportsToken($token, 'provider2'));
+    }
+
     /**
      * @param bool $isValidToken
      * @return \PHPUnit_Framework_MockObject_MockObject
      */
-    protected function getPamMock($isValidToken)
+    protected function getPamAuthenticatorMock($isValidToken)
     {
         $pamAuthenticatorMock = $this->getMockBuilder('AppBundle\Security\PamAuthenticator')
             ->setConstructorArgs([[]])

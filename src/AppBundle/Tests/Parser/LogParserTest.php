7e04cd1ff11681cdcb4714470c2620b61419c46b
spolischook <spolischook@gmail.com>
2015-11-21 08:07:09 +0200

    Added LogParserTest

---
+++ src/AppBundle/Tests/Parser/LogParserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Tests\Parser;
+
+use AppBundle\Parser\LogParser;
+
+class LogParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testParse()
+    {
+        $parser = new LogParser();
+        $entity = $parser->parse('127.0.0.1 - - [20/Nov/2015:16:22:39 +0200] "POST /login_check HTTP/1.1" 302 681');
+
+        $this->assertInstanceOf('AppBundle\Entity\Log', $entity);
+        $this->assertEquals('127.0.0.1', $entity->getHost());
+        $this->assertEquals('-', $entity->getLogname());
+        $this->assertEquals('-', $entity->getUser());
+        $this->assertEquals(strtotime('20/Nov/2015:16:22:39 +0200'), $entity->getStamp());
+        $this->assertEquals('20/Nov/2015:16:22:39 +0200', $entity->getTime());
+        $this->assertEquals('POST /login_check HTTP/1.1', $entity->getRequest());
+        $this->assertEquals('302', $entity->getStatus());
+        $this->assertEquals('681', $entity->getResponseBytes());
+    }
+}

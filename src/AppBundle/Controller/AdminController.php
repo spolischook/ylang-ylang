def56bda7e51256a48c42ead8857e5945feaf413
spolischook <spolischook@gmail.com>
2015-11-23 01:33:21 +0200

    Added form for Log search and filter

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,26 +2,42 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Dto\LogSearch;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
+    const PER_PAGE = 10;
+
     /**
      * @Route("/admin", name="admin")
      */
     public function indexAction(Request $request)
     {
+        $repository = $this->get('app.repository.log');
         $paginator  = $this->get('knp_paginator');
+
+        $form = $this->createForm('log_search_type', new LogSearch(), ['method' => 'GET']);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $searchLogQuery = $repository->getLogsSearchQuery($form->getData());
+        } else {
+            $searchLogQuery = $repository->getQueryBuilder();
+        }
+
         $pagination = $paginator->paginate(
-            $this->getDoctrine()->getManager()->getRepository('AppBundle:Log')->getLogsQuery(),
+            $searchLogQuery,
             $request->query->getInt('page', 1),
-            $request->query->getInt('limit', 10)
+            $request->query->getInt('limit', self::PER_PAGE)
         );
 
         return $this->render('admin/index.html.twig', [
             'pagination' => $pagination,
+            'form'       => $form->createView(),
+            'logSearch'  => $form->getData(),
         ]);
     }
 }

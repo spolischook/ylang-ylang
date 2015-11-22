4ffad73e208557a57c2d3d40ea2261afbbbdd7bb
spolischook <spolischook@gmail.com>
2015-11-22 05:16:39 +0200

    Added logs paginator with pager

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -4,16 +4,24 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('admin/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $this->getDoctrine()->getEntityManager()->getRepository('AppBundle:Log')->getLogsQuery(),
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 10)
+        );
+
+        return $this->render('admin/index.html.twig', [
+            'pagination' => $pagination,
+        ]);
     }
 }

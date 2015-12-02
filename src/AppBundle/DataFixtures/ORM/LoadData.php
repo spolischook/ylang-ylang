4ce8d13564d27f14b8fd244a16c4b9bd660438c1
spolischook <spolischook@gmail.com>
2015-12-03 00:45:01 +0200

    Removed container

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -5,10 +5,8 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Parser\LogParser;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadData implements FixtureInterface, ContainerAwareInterface
+class LoadData implements FixtureInterface
 {
     protected $usersFiles = [
         __DIR__."/../data/u1-d1.log"       => 'user1',
@@ -17,11 +15,6 @@ class LoadData implements FixtureInterface, ContainerAwareInterface
         __DIR__."/../data/u2-d2.log"       => "user2",
         __DIR__."/../data/root-access_log" => "admin"
     ];
-
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
     /**
      * Load data fixtures with the passed EntityManager
      *
@@ -29,7 +22,6 @@ class LoadData implements FixtureInterface, ContainerAwareInterface
      */
     public function load(ObjectManager $manager)
     {
-        $em = $this->container->get('doctrine.orm.entity_manager');
         $parser = new LogParser();
 
         foreach ($this->usersFiles as $file => $user) {
@@ -40,15 +32,6 @@ class LoadData implements FixtureInterface, ContainerAwareInterface
         }
 
         $em->flush();
-    }
 
-    /**
-     * Sets the container.
-     *
-     * @param ContainerInterface|null $container A ContainerInterface instance or null
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
     }
 }
